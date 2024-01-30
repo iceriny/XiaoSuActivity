638fb2e6aa4f4b29b1bfbf57acb422db3edb92a8
iceriny <suqianchengice@gmail.com>
2024-01-30 22:43:54 +0800

    完善框架

--- src/utiles.ts
+++ src/utiles.ts
@@ -4,5 +4,5 @@ export const bcModSDK = bcModSDKRef.registerMod({
 	name: "XiaoSuActivity",
 	fullName: "XiaoSu's Activity Expand",
 	version: XSActivity_VERSION.startsWith("v") ? XSActivity_VERSION.slice(1) : XSActivity_VERSION,
-	repository: "https://github.com/littlesera/LSCG"
+	repository: "https://github.com/iceriny/XiaoSuActivity"
 });
