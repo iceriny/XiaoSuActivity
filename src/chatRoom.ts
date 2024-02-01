c736c153daff503187e1aaa1b2f22f8d39eb6c96
iceriny <suqianchengice@gmail.com>
2024-02-02 03:31:37 +0800

    <test>

--- src/chatRoom.ts
+++ src/chatRoom.ts
@@ -1,10 +1,11 @@
 import { conDebug, hookFunction, ModuleCategory,MSGType } from "utils";
 
 hookFunction("ChatRoomRun", 30, (args, next) => {
-    next(args);
+    const result = next(args);
     conDebug({
         name: 'ChatRoomRunTest',
         type: MSGType.DebugLog,
         content: args
     });
+     return result;
 });
