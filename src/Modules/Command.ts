f3811de37c9bb142263acacfb067ca4c9082aa9e
iceriny <suqianchengice@gmail.com>
2024-02-02 17:12:58 +0800

    commands test

--- src/Modules/Command.ts
+++ src/Modules/Command.ts
@@ -1,9 +1,38 @@
 import { BaseModule } from "./BaseModule";
 import { conDebug, MSGType } from "utils";
 
+enum CommandLevel {
+    first = 1,
+    second = 2,
+    third = 3,
+}
 export class Commands extends BaseModule {
     moduleName = "Commands";
     priority = 20;
+
+    commandsDict: { [CommandLevel: string]: { [CommandName: string]: ICommand } } = {
+        'first': {
+            'help': {
+                Tag: "help",
+                Description: "显示 [小酥的活动模组] 的相关命令.",
+                Action: (args, msg, parsed) => {
+                    if (parsed.length > 0) this.CommandHandler(parsed);
+                    else this.DisplayHelp();
+                }
+            },
+            'export': {
+                Tag: "export",
+                Description: "导出当前聊天室的聊天记录.",
+                Action: (args, msg, parsed) => {
+
+                }
+            }
+        },
+        'second': {},
+        'third': {}
+    }
+
+
 
     public Load(): void {
         CommandCombine(
@@ -11,24 +40,45 @@ export class Commands extends BaseModule {
                 Tag: "xsa",
                 Description: "显示 [小酥的活动模组] 的相关命令.",
                 Action: (args, msg, parsed) => {
-                    conDebug(
-                        {
-                            name: "CommandsTest",
-                            type: MSGType.DebugLog,
-                            content: {
-                                "args": args,
-                                "msg": msg,
-                                "parsed": parsed
-                            }
-                        }
-                    )
+                    if (parsed.length > 0) this.CommandHandler(parsed);
+                    else this.DisplayHelp();
                 }
             }
         )
+    }
 
+    private DisplayHelp(commandLevel: CommandLevel = CommandLevel.first): void {
+        switch (commandLevel) {
+            case CommandLevel.first:
+                ChatRoomSendLocal(this.getHelpContent('first'), 5000);
+                break;
+            case CommandLevel.second:
+                ChatRoomSendLocal(this.getHelpContent('second'), 5000);
+                break;
+            case CommandLevel.third:
+                ChatRoomSendLocal(this.getHelpContent('third'), 5000);
+                break;
+        }
+    }
+    private getHelpContent(commandLevel: string) {
+        let content: string = ``;
+        for (const c in this.commandsDict[commandLevel]) {
+            content += `/xsa ${c} ${this.commandsDict[commandLevel][c].Description}\n`;
+        }
+        return content
+    }
 
+    private CommandHandler(parsed: Array<string>): void {
+        const parsedCount: number = parsed.length;
+        switch (parsedCount + 1) {
+            case 1:
+                this.DisplayHelp();
+                break;
+            case 2:
 
-
-        
+                break;
+            default:
+                this.DisplayHelp();
+        }
     }
 }
