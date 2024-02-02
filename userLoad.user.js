0a4eebd456743d0d616f2b81b359fdc18a3d72c1
iceriny <iceriny@users.noreply.github.com>
2024-02-02 19:27:22 +0000

    deploy: 532f537ae27ddc2b3ca22aa72624d4325f7e2f3d

---
+++ userLoad.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name 小酥的动作拓展
+// @namespace https://www.bondageprojects.com/
+// @version 0.0.1
+// @description 小酥的动作拓展 一些额外的动作
+// @author XiaoSu
+// @match https://bondageprojects.elementfx.com/*
+// @match https://www.bondageprojects.elementfx.com/*
+// @match https://bondage-europe.com/*
+// @match https://www.bondage-europe.com/*
+// @run-at document-end
+// @grant none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var script = document.createElement("script");
+    script.langauge = "JavaScript";
+    script.setAttribute("crossorigin", "anonymous");
+    script.src = `https://iceriny.github.io/XiaoSuActivity/XSActivity.js?${Date.now()}`;
+    document.head.appendChild(script);
+})();
