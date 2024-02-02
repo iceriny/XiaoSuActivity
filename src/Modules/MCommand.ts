d08ca3a56b7b624ae9f3235cff4b80af76ab846c
iceriny <suqianchengice@gmail.com>
2024-02-03 05:36:38 +0800

    -

--- src/Modules/MCommand.ts
+++ src/Modules/MCommand.ts
@@ -24,12 +24,12 @@ export class Commands extends BaseModule {
                 if (params == 'h') {
                     ChatRoomSendLocal('输入: ‘/xsa export -[时间]’导出指定时间范围内的聊天记录.\n例如: ‘/xsa export -05:34-20:40’\n默认导出当前聊天室的全部聊天记录.\n注意! \n如果时间段过长例如第一天的05:34到第二天的06:00则可能出现导出错误.')
                 } else if (params === '') {
-                    // 导出当前聊天室的全部聊天记录 --未实现
+                    // 导出当前聊天室的全部聊天记录
                     conDebug("导出当前聊天室的全部聊天记录");
                     (GetModule("Chatroom") as Chatroom).ExportChat();
                 }
                 else if (timeRangeRegex.test(params)) {
-                    // 导出指定时间段的聊天记录 --未实现
+                    // 导出指定时间段的聊天记录 
                     conDebug(`导出指定的 ${params} 时间段的聊天记录`);
                     const separatorIndex = params.indexOf('-');
 
