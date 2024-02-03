e8cb408f9e29c92e3776d0c46253f0915699a8e2
iceriny <suqianchengice@gmail.com>
2024-02-03 21:32:40 +0800

    完善push活动词条

--- src/Modules/MActivity.ts
+++ src/Modules/MActivity.ts
@@ -10,20 +10,10 @@ export class ActivityModule extends BaseModule {
     }
     Load(): void {
         this.LoadActivity();
-        hookFunction("ActivityDictionaryText", this.priority, (args, next) => {
-            const result = next(args);
-            conDebug({
-                name: 'ActivityDictionaryText',
-                type: MSGType.DebugLog,
-                content: {
-                    args: args,
-                    ActivityDictionary: ActivityDictionary
-                }
-            });
-            // SendChat("我是! 小酥的小白鼠! 吱吱吱吱~~~");
-            return result;
-        });
     }
+
+
+
     //============================================================
 
     LoadActivity(): void {
@@ -39,43 +29,6 @@ export class ActivityModule extends BaseModule {
         }
     }
     //============================================================
-    /**
-     * 创建活动对象的函数
-     * @param  name - 活动的名称
-     * @param  maxProgress - 活动的最大进度
-     * @param  maxProgressSelf - 活动自身的最大进度
-     * @param  prerequisite - 活动的前置条件
-     * @param  target - 活动的目标
-     * @param  targetSelf - 活动的自身目标
-     * @param  reverse - 是否反转前置条件的判断
-     * @param  makeSound - 是否播放声音 used for setting {@link ExtendedItemAutoPunishHandled} 
-     * @param  stimulationAction - 当使用该活动时触发的动作
-     * @param  activityExpression - 活动表达式,包含一系列的动作 该活动的默认表达式。可以使用资产上的ActivityExpression进行覆盖。
-     * @returns  - 包含创建的活动信息的对象
-     */
-    createActivity(
-        name: ActivityName,
-        maxProgress: number,
-        maxProgressSelf: number,
-        prerequisite: Array<ActivityPrerequisite>,
-        target: AssetGroupItemName,
-        targetSelf: AssetGroupItemName,
-        reverse?: true,
-        makeSound?: boolean,
-        stimulationAction?: StimulationAction,
-        activityExpression?: Array<ExpressionTrigger>): Activity {
-        const activity: Activity = {
-            Name: name,
-            Target: [target],
-            TargetSelf: [targetSelf],
-            MaxProgress: maxProgress,
-            MaxProgressSelf: maxProgressSelf,
-            Prerequisite: prerequisite,
-            ActivityExpression: activityExpression,
-        };
-        return activity;
-    }
-
     /**
      * 活动添加文字描述
      * @param name - 活动的名称
@@ -95,6 +48,7 @@ export class ActivityModule extends BaseModule {
             const addedValues = [];
 
             addedValues.push([`ActivityAct_${actName}`, `${actName}`]);
+            addedValues.push([`Activity${actName}`, `${actName}`]);
             if (actTarget.length > 0) {
                 addedValues.push([`Label-ChatOther-${actTarget}-${actName}`, `${actName}`]);
                 addedValues.push([`ChatOther-${actTarget}-${actName}`, pendingActivity.descSting[0]]);
@@ -116,6 +70,21 @@ export class ActivityModule extends BaseModule {
 
     //type ActivityNameXiaoSu = "眯眼" | "眼神飘忽" | "甩头发" | "轻抚发梢" | "叼起头发" | "嗅头发" | "皱鼻子" | "打喷嚏" | "深呼吸"
     // SourceCharacter 为动作发起人  TargetCharacter 为动作目标人
+    /**
+     * 将要添加的动作字典
+     * act : Activity Activity如下定义:
+     * @param  Name - 活动的名称
+     * @param  MaxProgress - 活动的最大进度
+     * @param  MaxProgressSelf - 活动自身的最大进度
+     * @param  Prerequisite - 活动的前置条件
+     * @param  Target - 活动的目标
+     * @param  TargetSelf - 活动的自身目标
+     * @param  Reverse - 是否反转前置条件的判断
+     * @param  MakeSound - 是否播放声音 used for setting {@link ExtendedItemAutoPunishHandled} 
+     * @param  StimulationAction - 当使用该活动时触发的动作
+     * @param  ActivityExpression - 活动表达式,包含一系列的动作 该活动的默认表达式。可以使用资产上的ActivityExpression进行覆盖。
+     * @interface Activity - 上述就是属性为活动的对象的属性
+    */
     activityToAddDict: { [ActivityName: string]: { act: Activity, desc: null | string[][], descSting: [string, string] } } = {
         squint: {
             act: {
@@ -124,8 +93,7 @@ export class ActivityModule extends BaseModule {
                 TargetSelf: ["ItemHead"],
                 MaxProgress: 20,
                 MaxProgressSelf: 20,
-                Prerequisite: [],
-                ActivityExpression: []
+                Prerequisite: []
             },
             desc: null,
             descSting: ["", "SourceCharacter眯了眯眼."]
