ed63e0335d81084048d469551c7c61b12a9c1c8a
iceriny <suqianchengice@gmail.com>
2024-02-07 00:33:22 +0800

    修复CHANGELOG错误

--- src/changeLogData.ts
+++ src/changeLogData.ts
@@ -1,4 +1,15 @@
-CHANGELOG = [
+enum ChangeType {
+	main,
+	dev
+}
+interface ChangeLogObj {
+	version: string;
+	description: string;
+	type: ChangeType;
+	changes: string[];
+}
+
+let CHANGELOG: ChangeLogObj[] = [
     {
         version: "0.0.1",
         description: "Initial release",
--- src/declaration/declaration.d.ts
+++ src/declaration/declaration.d.ts
@@ -1,18 +1,7 @@
 declare const XSActivity_VERSION: string;
 declare const DEBUG: boolean;
-declare let CHANGELOG: ChangeLogObj[];
 interface Window {
 	XSActivity_VERSION_Loaded?: boolean;
 	XSActivity_VERSION_Version?: string;
 }
 
-declare enum ChangeType {
-	main,
-	dev
-}
-interface ChangeLogObj {
-	version: string;
-	description: string;
-	type: ChangeType;
-	changes: string[];
-}
