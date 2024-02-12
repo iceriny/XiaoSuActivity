9985b4f47010e85d602306834a2d360b4a4742c5
iceriny <suqianchengice@gmail.com>
2024-02-12 23:08:02 +0800

    修复 patchFunction("ActivityOrgasmStart"

--- src/Modules/MArousal.ts
+++ src/Modules/MArousal.ts
@@ -38,13 +38,12 @@ export class ArousalModule extends BaseModule {
     patchListHandler(): void {
         // 处理OrgasmStart
         patchFunction("ActivityOrgasmStart",
-        {
+        {// XSA补丁处理~ 基础高潮时间为 4~7秒, 每边缘10秒钟增加随机的 500ms ~ 1000ms 的高潮时间。 最多增加 43000ms，也就是最长高潮时间为 50 秒
             "C.ArousalSettings.OrgasmTimer = CurrentTime + (Math.random() * 10000) + 5000;" : `if (window.EdgeCount === undefined) {
                 C.ArousalSettings.OrgasmTimer = CurrentTime + (Math.random() * 10000) + 5000;
             } else {
-                // XSA补丁处理~ 基础高潮时间为 4~7秒, 每边缘10秒钟增加随机的 500ms ~ 1000ms 的高潮时间。 最多增加 43000ms，也就是最长高潮时间为 50 秒
                 const addedTime = (Math.random() + 0.5) * window.EdgeCount * 1000;
-                C..ArousalSettings.OrgasmTimer = CurrentTime + (addedTime > 43000 ? 43000 : addedTime) + 4000 + (3000 * Math.random());
+                C.ArousalSettings.OrgasmTimer = CurrentTime + (addedTime > 43000 ? 43000 : addedTime) + 4000 + (3000 * Math.random());
             }`
         });
     }
