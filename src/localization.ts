fb78c2005715bf3ae4633bb3e191ea58dcfc9117
iceriny <suqianchengice@gmail.com>
2024-03-23 09:36:35 +0800

    添加获取翻译文件失败的处理逻辑

--- src/localization.ts
+++ src/localization.ts
@@ -4,31 +4,46 @@ export class Localization {
     private static readonly LINK: string = DEBUG ? 'https://iceriny.github.io/XiaoSuActivity/dev/' : 'https://iceriny.github.io/XiaoSuActivity/main/'
     public static STRINGS: IString;
 
-    public static async init() {
+    public static init() {
         hookFunction("TranslationNextLanguage", 0, (args, next) => {
             next(args);
             Localization.init();
         })
+        this.getLangJson();
+    }
 
 
+    private static getCount = 0;
+    private static getLangJson(langCode?: string) {
+        const L = langCode ? langCode : localStorage.getItem("BondageClubLanguage");
+        const lang = L ?? "CH";
 
-        const L = localStorage.getItem("BondageClubLanguage");
-        const lang = L ?? "EN";
-
-        const href = this.LINK + `${lang}.json`
+        const href = this.LINK + `${lang}.json`;
 
         fetch(href)
             .then((response) => {
                 return response.json();
             })
             .then((data) => {
-                Localization.STRINGS = data;
+                this.getCount = 0;
+                this.STRINGS = data;
             })
             .catch((error) => {
-                console.error(error);
+                this.getCount++;
+                if (this.getCount < 3) {
+                    console.error("获取翻译文件失败: ", error, "\n1秒后重新获取.");
+                    setTimeout(() => {
+                        this.getLangJson();
+                    }, 1000);
+                } else {
+                    if (lang === "CH") console.error("获取翻译文件失败: ", error, "\n3次失败.")
+                    else {
+                        console.error("获取翻译文件失败: ", error, "\n3次失败, 尝试获取默认的中文翻译.");
+                        this.getLangJson("CH");
+                    }
+                }
             });
     }
-
 
     public static get<T extends FirstStringKey>(firstKey: T, key: strKey<T>, ...param: unknown[]): string {
         return new STR(firstKey, key)
